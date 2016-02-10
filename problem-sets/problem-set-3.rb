ee79ede3362149867d1ff2fbe0bf8a826b8efcc5
Haseeb Qureshi <haseeb@haseebq.com>
2016-02-10 09:52:06 -0800

    Add d3 stuff and rename to parts

--- problem-sets/problem-set-3.rb
+++ problem-sets/problem-set-3.rb
@@ -1,5 +1,5 @@
 # ***********************************
-# Write a method that takes an array of numbers as input and uses select to return an array only of even numbers
+# Write a method that takes an array of numbers as input and uses #select to return an array only of even numbers
 
 def get_evens(array)
 end
@@ -10,7 +10,7 @@ puts get_evens([2, 4, 6, 8, 10, 12, 14]) == [2, 4, 6, 8, 10, 12, 14]
 puts get_evens([1, 3, 5, 7, 9, 11]) == []
 
 # ***********************************
-# Write a method that takes an array of numbers as input and uses reject to return an array of number that aren't odd
+# Write a method that takes an array of numbers as input and uses #reject to return an array of number that aren't odd
 
 def reject_odds(array)
 end
@@ -21,7 +21,7 @@ puts reject_odds([2, 4, 6, 8, 10) == [2, 4, 6, 8, 10]
 puts reject_odds([1.1, 3.5, 2.9]) == [1.1, 3.5, 2.9]
 
 # ************************************
-# Write a method that uses reduce to sum up the numbers in an array
+# Write a method that uses #reduce to sum up the numbers in an array
 
 def array_sum(array)
 end
@@ -30,3 +30,145 @@ puts "\narray sum"
 puts array_sum([]) == 0
 puts array_sum([1, 2, 3]) == 6
 puts array_sum([5, 5, 5, 5, 5]) == 25
+
+# ************************************
+# Write a method that takes an array of integers and returns an array of those values doubled.
+# This method should *not* modify the original array
+
+def calculate_doubles(array)
+end
+
+puts "\nCalculate Doubles"
+array = [1, 2, 3, 4, 5]
+doubled_array = calculate_doubles(array)
+puts array != doubled_array
+puts doubled_array == [2, 4, 6, 8, 10]
+
+# ************************************
+# Write a method that takes an array of integers and returns an array of those values doubled.
+# This method *should* modify the original array
+
+def calculate_doubles!(array)
+end
+
+puts "\nCalculate Doubles (bang)"
+array = [1, 2, 3, 4, 5]
+calculate_doubles!(array)
+puts array == [2, 4, 6, 8, 10]
+
+# ************************************
+# Write a method that takes an array of numbers as input and returns an array of each element in times its position within the array.
+# For example, [2, 9, 7] would return 23, since (2 * 0) + (9 * 1) + (7 * 2) = 0 + 9 + 14 = 23
+
+def array_sum_with_index(array)
+end
+
+puts "\nArray sum with index"
+puts array_sum_with_index([0, 1, 2, 3]) == 14
+
+# ************************************
+# Write a method that uses sort to implement the max function.  Your method should take an array as input and return
+# the element with the maximum value, calling sort once.
+
+def my_max(array)
+end
+
+puts "\nMy Max"
+puts my_max([5, 9, 2, 4, 7]) == 9
+puts my_max([1, 4, 9, 19, 3]) == 19
+
+# ************************************
+# Write a method that uses sort to implement the min function.  Your method should take an array as input and return
+# the element with the minimum value, calling sort once.
+
+def my_min(array)
+end
+
+puts "\nMy Min"
+puts my_min([5, 9, 2, 4, 7]) == 2
+puts my_min([0, -2, -5, -5, 1]) == -5
+
+# ************************************
+# Write a method that uses a loop and repeatedly uses Array#min and Array#push (or <<) to return an
+# array sorted from least to greatest.  Your method should not call Array#sort
+
+def my_sort_v1(array)
+end
+
+puts my_sort_v1([5, 2, 6, 3, 1, 4]) == [1, 2, 3, 4, 5, 6]
+
+# ************************************
+# Write a method that uses a loop and repeatedly uses Array#max and Array#unshift to return an array
+# sorted from least to greatest.  Your method should not call Array#sort
+
+def my_sort_v2(array)
+end
+
+puts my_sort_v2([5, 2, 6, 3, 1, 4]) == [1, 2, 3, 4, 5, 6]
+
+# ************************************
+# Write a method that returns the third greatest element in an array
+
+def third_greatest(array)
+end
+
+puts third_greatest([5, 9, 3, 7, 7, 2, 10]) == 7
+
+# ************************************
+# Write a method that takes an array and a number and repeatedly uses delete_at to delete that many elements from the
+# front of the array
+
+def delete_first(array, n)
+end
+
+puts "\nDelete first"
+p delete_first([1, 2, 3, 4, 5, 6], 1) == [2, 3, 4, 5, 6]
+p delete_first([1, 2, 3, 4, 5, 6], 3) == [4, 5, 6]
+
+# ************************************
+# Write a method that takes a string of words separated by spaces and returns the longest word.  If there is more
+# than one word of that length, it should return the first instance of that word.
+
+def longest_word(string)
+end
+
+puts "\nLongest Word"
+puts longest_word("we the people in order to form a more perfect union establish justice ensure domestic tranquility") == "tranquility"
+puts longest_word("one two three four five six seven") == "three"
+
+# ************************************
+# Write a method that takes a string and counts the number of vowels in the string.  For our purposes, "y" counts as a vowel.
+# You might want to use the Array#include? method for this.  The usage is:
+# [1, 2, 3, 4, 5].include?(2) => true
+# [1, 2, 3, 4, 5].include?("k") => false
+# Assume all letters will be lower case
+
+def count_vowels(string)
+end
+
+puts "\nCount Vowels"
+puts count_vowels("cat dog elephant monkey") == 8
+puts count_vowels("abcdefghijklmnopqrstuvwxyz") == 6
+
+# ************************************
+# Write a method that takes a string of words separated by spaces and returns a string the same as the original, but
+# with five-letter words replaced with "#####"
+
+def redact_five_letter_words(string)
+end
+
+puts "\nRedact Five Letter Words"
+puts redact_five_letter_words("one two three four five six seven eight") == "one two ##### four five six ##### eight"
+puts redact_five_letter_words("we the people in order to form a more perfect union establish justice ensure domestic tranquility") == "we the people in ##### to form a more perfect ##### establish justice ensure domestic tranquility"
+
+# ************************************
+# Write a method that takes a string of words separated by spaces and returns the most common vowel.  If more than one vowel has that count,
+# return the one that occurs earliest in the alphabet.  For our purposes, count "y" as a vowel.
+# Assume all letters are lower case.
+
+def most_common_vowel(str)
+end
+
+puts "\nMost Common Vowel"
+puts most_common_vowel("we the people in order to form a more perfect union establish justice ensure domestic tranquility") == "e"
+puts most_common_vowel("cat dog octopus spider") == "o"
