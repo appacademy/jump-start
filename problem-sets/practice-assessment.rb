8e53bec8a85879a665af156b7919f618d3c80b17
Asher King Abramson <akingabramson@appacademy.io>
2016-03-30 23:23:45 -0700

    Improve xbonnacci instructions

--- problem-sets/practice-assessment.rb
+++ problem-sets/practice-assessment.rb
@@ -96,9 +96,17 @@ puts isogram_matcher("unpredictably", "hydromagnetic") == [1, 8]
 
 # Xbonacci
 # ------------------------------------------------------------------------------
-# Write a Xbonacci function (that works similarly to Fibonacci) where the sum of
-# the last X numbers (instead of the last 2 numbers) of the sequence becomes the
-# next number in the sequence.
+
+# Write a Xbonacci function that works similarly to the fibonacci sequence.
+# The fibonacci sequence takes the last two numbers in the sequence and adds
+# them together to create the next number.
+#
+# First five fibonnacci numbers = [1, 1, 2, 3, 5]
+# The fourth fibonnacci number (3) is the sum of the two numbers before it
+# (1 and 2).
+#
+# In Xbonnacci, the sum of the last X numbers (instead of the last 2 numbers)
+# of the sequence becomes the next number in the sequence.
 #
 # The method will take two inputs: the starting sequence with X number of
 # elements and an integer N, and return the first N elements in the given
