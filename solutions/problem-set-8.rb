d41cc8406c9ea889a0cba9dbe2cce6d14319c9d4
Asher King Abramson <akingabramson@appacademy.io>
2016-04-05 22:30:36 -0700

    Clarify number counting sequence instructions

--- solutions/problem-set-8.rb
+++ solutions/problem-set-8.rb
@@ -388,6 +388,8 @@ end
 # "11" which is counted as "two 1s" or "21", followed by
 # "21" which is counted as "one 2 and one 1" or "1211", and so on.
 #
+# It helps to "say it out loud" to describe the item in the sequence.
+#
 # Given an integer n, generate the n-th element in the number counting sequence.
 
 def number_counting_seq(n)
@@ -428,8 +430,10 @@ end
 # puts "-------Number Counting Sequence-------"
 # puts number_counting_seq(0) == ""
 # puts number_counting_seq(1) == "1"
-# puts number_counting_seq(2) == "11"
-# puts number_counting_seq(3) == "21"
-# puts number_counting_seq(5) == "111221"
+# puts number_counting_seq(2) == "11" # one 1 above
+# puts number_counting_seq(3) == "21" # two 1s above
+# # ...
+# puts number_counting_seq(5) == "111221" #the last sequence was one 1, one 2, and two 1s: "1211"
 # puts number_counting_seq(8) == "1113213211"
 # puts number_counting_seq(10) == "13211311123113112211"
+
