c74d4a18ceee6dc79d75b5ada3e992ab87409be8
Ryan <Hall.Ryan.R@gmail.com>
2016-08-31 15:35:15 -0700

    refactor problem sets

--- jumpstart-files/problem-sets/problem-set-6.rb
+++ jumpstart-files/problem-sets/problem-set-6.rb
@@ -38,38 +38,6 @@ puts word_with_most_repeats('what if there is a tie betwixt words') == 'there'
 puts word_with_most_repeats('ooooooooooh tutu') == 'tutu'
 
 
-# Even Splitters
-# ------------------------------------------------------------------------------
-# Return an array of characters on which we can split an input string to produce
-# substrings of the same length.
-
-# Don't count empty strings after the split.
-
-# Here's an example for "banana":
-#
-# "banana".split("a") # => ["b", "n", "n"] (all elements same length)
-# "banana".split("b") # => ["", anana"] (all elements same length - there's only
-# one element "anana" because the empty string doesn't count)
-# "banana".split("n") # => ["ba", "a", "a"] (all elements NOT same length)
-#
-# result: ["b", "a"]
-#
-# ------------------------------------------------------------------------------
-
-def even_splitters(string)
-  #
-  # your code goes here
-  #
-end
-
-puts "-----Even Splitters----"
-puts even_splitters("") == []
-puts even_splitters("t") == ["t"]
-puts even_splitters("jk") == ["j", "k"]
-puts even_splitters("xoxo") == ["x", "o"]
-puts even_splitters("banana") == ["b","a"]
-puts even_splitters("mishmash") == ["m","h"]
-
 
 # Isogram Matcher
 # ------------------------------------------------------------------------------
--- jumpstart-files/problem-sets/problem-set-8.rb
+++ jumpstart-files/problem-sets/problem-set-8.rb
@@ -267,35 +267,3 @@ care_bear_counts_2 = {
 
 puts care_bear_summary(hug_calendar_1) == care_bear_counts_1
 puts care_bear_summary(hug_calendar_2) == care_bear_counts_2
-
-
-# Number Counting Sequence
-# ------------------------------------------------------------------------------
-# The number counting sequence is a sequence of number strings that begins with:
-# ["1", "11", "21", "1211", "111221", ...]
-#
-# "1" is counted as "one 1" or "11", followed by
-# "11" which is counted as "two 1s" or "21", followed by
-# "21" which is counted as "one 2 and one 1" or "1211", and so on.
-#
-# It helps to "say it out loud" to describe the item in the sequence.
-#
-# Given an integer n, generate the n-th element in the number counting sequence.
-
-def number_counting_seq(n)
-  #
-  # your code goes here
-  #
-end
-
-
-
-puts "-------Number Counting Sequence-------"
-puts number_counting_seq(0) == ""
-puts number_counting_seq(1) == "1"
-puts number_counting_seq(2) == "11" # one 1 above
-puts number_counting_seq(3) == "21" # two 1s above
-# ...
-puts number_counting_seq(5) == "111221" #the last sequence was one 1, one 2, and two 1s: "1211"
-puts number_counting_seq(8) == "1113213211"
-puts number_counting_seq(10) == "13211311123113112211"
